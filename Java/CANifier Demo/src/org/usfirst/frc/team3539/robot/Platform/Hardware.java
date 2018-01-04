d814d6829db22a8fb15b0dc737db3362b4a42f51
CoryNessCTR <cnessctre@gmail.com>
2018-01-04 16:44:33 -0500

    First commit of Out-Of-Date
    Contains all the projects that are currently out of date for the firmware or software library.

--- Java/CANifier Demo/src/org/usfirst/frc/team3539/robot/Platform/Hardware.java
+++
@@ -1,9 +0,0 @@
-package org.usfirst.frc.team3539.robot.Platform;
-
-import com.ctre.phoenix.CANifier;
-import edu.wpi.first.wpilibj.Joystick;
-
-public class Hardware {
-	public static CANifier canifier = new CANifier(0);
-	public static Joystick gamepad = new Joystick(0);
-}
