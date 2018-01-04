276c80d555e7242294c6e5e391e13542f8915108
CoryNessCTR <cnessctre@gmail.com>
2018-01-04 16:45:44 -0500

    Removed out of date projects
    Out of date projects are now in branch Out-Of-Date

--- C++/RC Mecanum Bot/src/Platform/Schedulers.cpp
+++
@@ -1,3 +0,0 @@
-#include "Platform/Schedulers.h"
-
-CTRE::Tasking::Schedulers::ConcurrentScheduler* Schedulers::PeriodicTasks = new CTRE::Tasking::Schedulers::ConcurrentScheduler();
--- C++/RC Mecanum Bot/src/Platform/Tasks.cpp
+++
@@ -1,7 +0,0 @@
-#include "Platform/Tasks.h"
-
-TaskEnableRobot* Tasks::EnableRobot = new TaskEnableRobot();
-TaskLEDStrip* Tasks::LEDStrip = new TaskLEDStrip();
-TaskTeleopDriveWithRC* Tasks::TeleopDriveWithRC = new TaskTeleopDriveWithRC();
-TaskTeleopDriveWithGamepad* Tasks::TeleopDriveWithGamepad = new TaskTeleopDriveWithGamepad();
-TaskLowBatteryDetect* Tasks::LowBatteryDetect = new TaskLowBatteryDetect();
