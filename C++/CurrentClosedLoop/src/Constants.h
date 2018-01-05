e42e9ed041bd1950dcc57f35623d943dfa844ee1
CoryNessCTR <cnessctre@gmail.com>
2018-01-05 14:11:04 -0500

    Current Closed Loop Added and Tested
    Current Closed Loop is added to both Java and C++ examples.

---
+++ C++/CurrentClosedLoop/src/Constants.h
@@ -0,0 +1,25 @@
+
+#ifndef SRC_CONSTANTS_H_
+#define SRC_CONSTANTS_H_
+
+enum Constants
+{
+	/**
+	 * Which PID slot to pull gains from.  Starting 2018, you can choose
+	 * from 0,1,2 or 3.  Only the first two (0,1) are visible in web-based configuration.
+	 */
+	 kSlotIdx = 0,
+
+	/* Talon SRX/ Victor SPX will supported multiple (cascaded) PID loops.
+	 * For now we just want the primary one.
+	 */
+	 kPIDLoopIdx = 0,
+
+	/*
+	 * set to zero to skip waiting for confirmation, set to nonzero to wait
+	 * and report to DS if action fails.
+	 */
+	 kTimeoutMs = 10
+};
+
+#endif
