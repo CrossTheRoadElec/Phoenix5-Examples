640c8c90ebf8c69d52278d5d56dacd88f3c1bb33
Omar Zrien <Ozrien@crosstheroadelectronics.com>
2018-01-23 22:06:20 -0500

    Whitespace changes, comment tweaks

--- Java/MotionProfile/src/org/usfirst/frc/team217/robot/Constants.java
+++ Java/MotionProfile/src/org/usfirst/frc/team217/robot/Constants.java
@@ -1,43 +1,43 @@
 package org.usfirst.frc.team217.robot;
 
 public class Constants {
-	
-	/** which Talon on CANBus*/
+
+	/** which Talon on CANBus */
 	public static final int kTalonID = 0;
 
 	/**
-	 * How many sensor units per rotation.
- 	 * Using CTRE Magnetic Encoder.
+	 * How many sensor units per rotation. Using CTRE Magnetic Encoder.
+	 * 
 	 * @link https://github.com/CrossTheRoadElec/Phoenix-Documentation#what-are-the-units-of-my-sensor
 	 */
 	public static final double kSensorUnitsPerRotation = 4096;
 
 	/**
-	 * Which PID slot to pull gains from.  Starting 2018, you can choose 
-	 * from 0,1,2 or 3.  Only the first two (0,1) are visible in web-based configuration.
+	 * Which PID slot to pull gains from. Starting 2018, you can choose from
+	 * 0,1,2 or 3. Only the first two (0,1) are visible in web-based
+	 * configuration.
 	 */
 	public static final int kSlotIdx = 0;
-	
-	/** 
-	 * Talon SRX/ Victor SPX will supported multiple (cascaded) PID loops.  
-	 * For now we just want the primary one.
+
+	/**
+	 * Talon SRX/ Victor SPX will supported multiple (cascaded) PID loops. For
+	 * now we just want the primary one.
 	 */
 	public static final int kPIDLoopIdx = 0;
 	/**
-	 * set to zero to skip waiting for confirmation, set to nonzero to wait
-	 * and report to DS if action fails.
+	 * set to zero to skip waiting for confirmation, set to nonzero to wait and
+	 * report to DS if action fails.
 	 */
 	public static final int kTimeoutMs = 10;
-	
+
 	/**
-	 * Base trajectory period to add to each individual 
-	 * trajectory point's unique duration.  This can be set
-	 * to any value within [0,255]ms.
+	 * Base trajectory period to add to each individual trajectory point's
+	 * unique duration. This can be set to any value within [0,255]ms.
 	 */
 	public static final int kBaseTrajPeriodMs = 0;
-	
+
 	/**
 	 * Motor deadband, set to 1%.
 	 */
-	public static final double kNeutralDeadband  = 0.01;
+	public static final double kNeutralDeadband = 0.01;
 }
--- Java/MotionProfile/src/org/usfirst/frc/team217/robot/Robot.java
+++ Java/MotionProfile/src/org/usfirst/frc/team217/robot/Robot.java
@@ -37,21 +37,22 @@ public class Robot extends IterativeRobot {
 
 	/** The Talon we want to motion profile. */
 	TalonSRX _talon = new TalonSRX(Constants.kTalonID);
-	
+
 	/** some example logic on how one can manage an MP */
 	MotionProfileExample _example = new MotionProfileExample(_talon);
-	
+
 	/** joystick for testing */
-	Joystick _joy= new Joystick(0);
+	Joystick _joy = new Joystick(0);
 
-	/** cache last buttons so we can detect press events.  In a command-based project you can leverage the on-press event
-	 * but for this simple example, lets just do quick compares to prev-btn-states */
-	boolean [] _btnsLast = {false,false,false,false,false,false,false,false,false,false};
-
-
+	/**
+	 * cache last buttons so we can detect press events. In a command-based
+	 * project you can leverage the on-press event but for this simple example,
+	 * lets just do quick compares to prev-btn-states
+	 */
+	boolean[] _btnsLast = {false, false, false, false, false, false, false, false, false, false};
 
 	/** run once after booting/enter-disable */
-	public void disabledInit() { 
+	public void disabledInit() {
 
 		_talon.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative, 0, 10);
 		_talon.setSensorPhase(true); /* keep sensor and motor in phase */
@@ -60,67 +61,83 @@ public class Robot extends IterativeRobot {
 		_talon.config_kF(0, 0.076, Constants.kTimeoutMs);
 		_talon.config_kP(0, 2.000, Constants.kTimeoutMs);
 		_talon.config_kI(0, 0.0, Constants.kTimeoutMs);
-		_talon.config_kD(0,20.0, Constants.kTimeoutMs);
+		_talon.config_kD(0, 20.0, Constants.kTimeoutMs);
 
-		_talon.configMotionProfileTrajectoryPeriod(10, Constants.kTimeoutMs); //Our profile uses 10 ms timing
-		/* status 10 provides the trajectory target for motion profile AND motion magic */
+		/* Our profile uses 10ms timing */
+		_talon.configMotionProfileTrajectoryPeriod(10, Constants.kTimeoutMs); 
+		/*
+		 * status 10 provides the trajectory target for motion profile AND
+		 * motion magic
+		 */
 		_talon.setStatusFramePeriod(StatusFrameEnhanced.Status_10_MotionMagic, 10, Constants.kTimeoutMs);
 	}
-	/**  function is called periodically during operator control */
-    public void teleopPeriodic() {
+
+	/** function is called periodically during operator control */
+	public void teleopPeriodic() {
 		/* get buttons */
-		boolean [] btns= new boolean [_btnsLast.length];
-		for(int i=1;i<_btnsLast.length;++i)
+		boolean[] btns = new boolean[_btnsLast.length];
+		for (int i = 1; i < _btnsLast.length; ++i)
 			btns[i] = _joy.getRawButton(i);
 
 		/* get the left joystick axis on Logitech Gampead */
 		double leftYjoystick = -1 * _joy.getY(); /* multiple by -1 so joystick forward is positive */
 
-		/* call this periodically, and catch the output.  Only apply it if user wants to run MP. */
+		/*
+		 * call this periodically, and catch the output. Only apply it if user
+		 * wants to run MP.
+		 */
 		_example.control();
-		
-		if (btns[5] == false) { /* Check button 5 (top left shoulder on the logitech gamead). */
+
+		/* Check button 5 (top left shoulder on the logitech gamead). */
+		if (btns[5] == false) {
 			/*
-			 * If it's not being pressed, just do a simple drive.  This
-			 * could be a RobotDrive class or custom drivetrain logic.
-			 * The point is we want the switch in and out of MP Control mode.*/
-		
+			 * If it's not being pressed, just do a simple drive. This could be
+			 * a RobotDrive class or custom drivetrain logic. The point is we
+			 * want the switch in and out of MP Control mode.
+			 */
+
 			/* button5 is off so straight drive */
 			_talon.set(ControlMode.PercentOutput, leftYjoystick);
 
 			_example.reset();
 		} else {
-			/* Button5 is held down so switch to motion profile control mode => This is done in MotionProfileControl.
-			 * When we transition from no-press to press,
-			 * pass a "true" once to MotionProfileControl.
+			/*
+			 * Button5 is held down so switch to motion profile control mode =>
+			 * This is done in MotionProfileControl. When we transition from
+			 * no-press to press, pass a "true" once to MotionProfileControl.
 			 */
-			
+
 			SetValueMotionProfile setOutput = _example.getSetValue();
-					
+
 			_talon.set(ControlMode.MotionProfile, setOutput.value);
 
-			/* if btn is pressed and was not pressed last time,
-			 * In other words we just detected the on-press event.
-			 * This will signal the robot to start a MP */
-			if( (btns[6] == true) && (_btnsLast[6] == false) ) {
+			/*
+			 * if btn is pressed and was not pressed last time, In other words
+			 * we just detected the on-press event. This will signal the robot
+			 * to start a MP
+			 */
+			if ((btns[6] == true) && (_btnsLast[6] == false)) {
 				/* user just tapped button 6 */
 
-				//------------ We could start an MP if MP isn't already running ------------//
+				// --- We could start an MP if MP isn't already running ----//
 				_example.startMotionProfile();
 			}
 		}
 
 		/* save buttons states for on-press detection */
-		for(int i=1;i<10;++i)
+		for (int i = 1; i < 10; ++i)
 			_btnsLast[i] = btns[i];
 
 	}
-	/**  function is called periodically during disable */
+
+	/** function is called periodically during disable */
 	public void disabledPeriodic() {
-		/* it's generally a good idea to put motor controllers back
-		 * into a known state when robot is disabled.  That way when you
-		 * enable the robot doesn't just continue doing what it was doing before.
-		 * BUT if that's what the application/testing requires than modify this accordingly */
+		/*
+		 * it's generally a good idea to put motor controllers back into a known
+		 * state when robot is disabled. That way when you enable the robot
+		 * doesn't just continue doing what it was doing before. BUT if that's
+		 * what the application/testing requires than modify this accordingly
+		 */
 		_talon.set(ControlMode.PercentOutput, 0);
 		/* clear our buffer and put everything into a known state */
 		_example.reset();
