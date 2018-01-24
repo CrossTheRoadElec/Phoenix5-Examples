792c0a49a01b41448ee43d61aa483ec8cfc0511e
Omar Zrien <Ozrien@crosstheroadelectronics.com>
2018-01-23 20:56:58 -0500

    Comments and white space changes

--- Java/MagEncoder/src/org/usfirst/frc/team217/robot/Robot.java
+++ Java/MagEncoder/src/org/usfirst/frc/team217/robot/Robot.java
@@ -16,9 +16,9 @@
  * SmartDashboard (SD) setup.
  * [1] Open Smartdashboard (I typically (re)select the Dashboard Type in DriverStation if the SD doesn't pop up).
  * [2] Deploy software and enable.
- * [3] Find the text entry in the SD for "spd".  
+ * [3] Find the text entry in the SD for "vel".  
  * [4] View =>Editable should be checked.
- * [5] Right-click on "spd" label and "Change to..." the Line Plot.  
+ * [5] Right-click on "vel" label and "Change to..." the Line Plot.  
  * 
  * A few details regarding Smartdashboard in general...
  * [1] Constant data does not render new plot points. So if the signal being measured doesn't change value, the plot stops.
@@ -31,7 +31,7 @@
  *  
  * @author Ozrien
  */
-package org.usfirst.frc.team217.robot; // bulldogs!
+package org.usfirst.frc.team217.robot;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.FeedbackDevice;
@@ -39,61 +39,86 @@ import com.ctre.phoenix.motorcontrol.StatusFrameEnhanced;
 import com.ctre.phoenix.motorcontrol.can.*;
 
 import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Robot extends IterativeRobot {
-	
-	VictorSPX _vic = new VictorSPX(1); //!< Just a follower
-	TalonSRX _tal = new TalonSRX(3);	
+
+	/**
+	 * Just a follower, could be a Talon.
+	 */
+	VictorSPX _vic = new VictorSPX(1);
+	/**
+	 * Master Talon
+	 */
+	TalonSRX _tal = new TalonSRX(2);
+	/**
+	 * Simple thread to plot the sensor velocity
+	 */
 	PlotThread _plotThread;
-	
+	/**
+	 * joystick or gamepad
+	 */
+	Joystick _joystick = new Joystick(0);
 	
 	public void teleopInit() {
-		/* Tal1 will follow Tal3 */
+		/* Victor will follow Talon */
 		_vic.follow(_tal);
-		
-		/* new frame every 1ms, since this is a test project use up as 
-		 * much bandwidth as possible for the purpose of this test. */
-		_tal.setStatusFramePeriod(StatusFrameEnhanced.Status_2_Feedback0, 1, 10); 
+
+		/*
+		 * new frame every 1ms, since this is a test project use up as much
+		 * bandwidth as possible for the purpose of this test.
+		 */
+		_tal.setStatusFramePeriod(StatusFrameEnhanced.Status_2_Feedback0, 1, 10);
 		_tal.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative, 0, 10);
-		
+
 		/* fire the plotter */
 		_plotThread = new PlotThread(this);
 		new Thread(_plotThread).start();
 	}
 
 	public void teleopPeriodic() {
-		/* Shooting for ~200RPM, which is ~300ms per rotation.
-		 *   
-		 * If there is mechanical deflection, eccentricity, or damage in the sensor
-		 * it should be revealed in the plot.  
+		/*
+		 * If there is mechanical deflection, eccentricity, or damage in the
+		 * sensor it should be revealed in the plot.
 		 * 
-		 * For example, an optical encoder with a partially damaged ring will reveal a 
-		 * periodic dip in the sensed velocity synchronous with each rotation.
+		 * For example, an optical encoder with a partially damaged ring will
+		 * reveal a periodic dip in the sensed velocity synchronous with each
+		 * rotation.
 		 * 
-		 *  This can also be wired to a gamepad to test velocity sweeping.
-		 * */
-		_tal.set(ControlMode.PercentOutput, 0.4);		
+		 * This can also be wired to a gamepad to test velocity sweeping.
+		 */
+		if (_joystick.getRawButton(1))
+			_tal.set(ControlMode.PercentOutput, 0.25); /* 25 % output */
+		else 
+			_tal.set(ControlMode.PercentOutput, 0.0);
 	}
-	
+
 	/** quick and dirty threaded plotter */
 	class PlotThread implements Runnable {
-		 Robot robot;
-		 public PlotThread(Robot robot) { this.robot = robot; }
+		Robot robot;
 
-		 public void run() {
-			/* speed up network tables, this is a test project so eat up all 
-			 * of the network possible for the purpose of this test.
+		public PlotThread(Robot robot) {
+			this.robot = robot;
+		}
+
+		public void run() {
+			/*
+			 * speed up network tables, this is a test project so eat up all of
+			 * the network possible for the purpose of this test.
 			 */
-			//NetworkTable.setUpdateRate(0.010); /* this suggests each time unit is 10ms in the plot */
-			 while (true) {
+			// NetworkTable.setUpdateRate(0.010); /* this suggests each time
+			// unit is 10ms in the plot */
+			while (true) {
 				/* yield for a ms or so - this is not meant to be accurate */
-				try { Thread.sleep(1); } catch (Exception e) { }
+				try {
+					Thread.sleep(1);
+				} catch (Exception e) {
+				}
 				/* grab the last signal update from our 1ms frame update */
-				double speed = this.robot._tal.getSelectedSensorVelocity(0);
-				SmartDashboard.putNumber("spd", speed);
+				double velocity = this.robot._tal.getSelectedSensorVelocity(0);
+				SmartDashboard.putNumber("vel", velocity);
 			}
 		}
 	}
 }
-
