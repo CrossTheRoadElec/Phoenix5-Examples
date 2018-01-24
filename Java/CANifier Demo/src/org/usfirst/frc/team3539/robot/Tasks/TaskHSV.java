a1932738a9ee44097106a7d036ee4ba36d9fe9b6
Omar Zrien <Ozrien@crosstheroadelectronics.com>
2018-01-23 22:21:54 -0500

    CANifier demo added

--- Java/CANifier Demo/src/org/usfirst/frc/team3539/robot/Tasks/TaskHSV.java
+++ Java/CANifier Demo/src/org/usfirst/frc/team3539/robot/Tasks/TaskHSV.java
@@ -20,12 +20,15 @@ public class TaskHSV implements ILoopable {
 	public void onStart() {
 
 	}
+
 	public void onStop() {
 
 	}
+
 	public boolean isDone() {
 		return false;
 	}
+
 	public void onLoop() {
 		if (Saturation > 1) {
 			Saturation = 1;
@@ -40,14 +43,17 @@ public class TaskHSV implements ILoopable {
 
 		/* Convert to HSV to RGB */
 		_rgb = HsvToRgb.convert(Hue, Saturation, Value);
-		
+
 		_rgb[0] = _averageR.process(_rgb[0]);
 		_rgb[1] = _averageG.process(_rgb[1]);
 		_rgb[2] = _averageB.process(_rgb[2]);
 
 		/* Update CANifier's LED strip */
-		Hardware.canifier.setLEDOutput(_rgb[0], CANifier.LEDChannel.LEDChannelA);
-		Hardware.canifier.setLEDOutput(_rgb[1], CANifier.LEDChannel.LEDChannelB);
-		Hardware.canifier.setLEDOutput(_rgb[2], CANifier.LEDChannel.LEDChannelC);
+		Hardware.canifier.setLEDOutput(_rgb[0],
+				CANifier.LEDChannel.LEDChannelA);
+		Hardware.canifier.setLEDOutput(_rgb[1],
+				CANifier.LEDChannel.LEDChannelB);
+		Hardware.canifier.setLEDOutput(_rgb[2],
+				CANifier.LEDChannel.LEDChannelC);
 	}
 }
--- Java/CANifier Demo/src/org/usfirst/frc/team3539/robot/Tasks/TaskMainLoop.java
+++ Java/CANifier Demo/src/org/usfirst/frc/team3539/robot/Tasks/TaskMainLoop.java
@@ -5,59 +5,54 @@ import org.usfirst.frc.team3539.robot.Platform.Schedulers;
 import org.usfirst.frc.team3539.robot.Platform.Hardware;
 import org.usfirst.frc.team3539.robot.Platform.Tasks;
 
-public class TaskMainLoop implements ILoopable{
+public class TaskMainLoop implements ILoopable {
 	/* ILoopable */
-    public void onStart()
-    {
-        /* Default to LED strip animation */
-        Schedulers.PeriodicTasks.start(Tasks.taskAnimateLEDStrip);
-        Schedulers.PeriodicTasks.stop(Tasks.taskDirectControlArm);
-        Schedulers.PeriodicTasks.stop(Tasks.taskLIDAR_ControlLEDStrip);
-    }
-    public void onStop() {
-    	
-    }
-    public boolean isDone() {
-    	return false;
-    }
-	public void onLoop()
-    {
-        boolean gamepadPresent = false;
-    	/* Don't have the ability to check if game-pad is connected, manually enable */
-        if (gamepadPresent)
-        {
-            Schedulers.PeriodicTasks.start(Tasks.taskPWMmotorController);
-        }
-        else
-        {
-            Schedulers.PeriodicTasks.stop(Tasks.taskPWMmotorController);
-        }
+	public void onStart() {
+		/* Default to LED strip animation */
+		Schedulers.PeriodicTasks.start(Tasks.taskAnimateLEDStrip);
+		Schedulers.PeriodicTasks.stop(Tasks.taskDirectControlArm);
+		Schedulers.PeriodicTasks.stop(Tasks.taskLIDAR_ControlLEDStrip);
+	}
 
+	public void onStop() {
 
-        if (Hardware.gamepad.getRawButton(6))
-        {
-            /* Roll through color wheel*/
-            Schedulers.PeriodicTasks.start(Tasks.taskAnimateLEDStrip);
-            Schedulers.PeriodicTasks.stop(Tasks.taskDirectControlArm);
-            Schedulers.PeriodicTasks.stop(Tasks.taskLIDAR_ControlLEDStrip);
-        }
-        else if (Hardware.gamepad.getRawButton(5))
-        {
-            /* Let user control LED with sticks */
-            Schedulers.PeriodicTasks.stop(Tasks.taskAnimateLEDStrip);
-            Schedulers.PeriodicTasks.start(Tasks.taskDirectControlArm);
-            Schedulers.PeriodicTasks.stop(Tasks.taskLIDAR_ControlLEDStrip);
+	}
 
-            Schedulers.PeriodicTasks.start(Tasks.taskMeasurePulseSensors);
-        }
-        else if (Hardware.gamepad.getRawButton(7))
-        {
-        	/* LED's controlled with the use of LIDAR sensor */
-            Schedulers.PeriodicTasks.stop(Tasks.taskAnimateLEDStrip);
-            Schedulers.PeriodicTasks.stop(Tasks.taskDirectControlArm);
-            Schedulers.PeriodicTasks.start(Tasks.taskLIDAR_ControlLEDStrip);
+	public boolean isDone() {
+		return false;
+	}
 
-            Schedulers.PeriodicTasks.start(Tasks.taskMeasurePulseSensors);
-        }
-    }
+	public void onLoop() {
+		boolean gamepadPresent = false;
+		/*
+		 * Don't have the ability to check if game-pad is connected, manually
+		 * enable
+		 */
+		if (gamepadPresent) {
+			Schedulers.PeriodicTasks.start(Tasks.taskPWMmotorController);
+		} else {
+			Schedulers.PeriodicTasks.stop(Tasks.taskPWMmotorController);
+		}
+
+		if (Hardware.gamepad.getRawButton(6)) {
+			/* Roll through color wheel */
+			Schedulers.PeriodicTasks.start(Tasks.taskAnimateLEDStrip);
+			Schedulers.PeriodicTasks.stop(Tasks.taskDirectControlArm);
+			Schedulers.PeriodicTasks.stop(Tasks.taskLIDAR_ControlLEDStrip);
+		} else if (Hardware.gamepad.getRawButton(5)) {
+			/* Let user control LED with sticks */
+			Schedulers.PeriodicTasks.stop(Tasks.taskAnimateLEDStrip);
+			Schedulers.PeriodicTasks.start(Tasks.taskDirectControlArm);
+			Schedulers.PeriodicTasks.stop(Tasks.taskLIDAR_ControlLEDStrip);
+
+			Schedulers.PeriodicTasks.start(Tasks.taskMeasurePulseSensors);
+		} else if (Hardware.gamepad.getRawButton(7)) {
+			/* LED's controlled with the use of LIDAR sensor */
+			Schedulers.PeriodicTasks.stop(Tasks.taskAnimateLEDStrip);
+			Schedulers.PeriodicTasks.stop(Tasks.taskDirectControlArm);
+			Schedulers.PeriodicTasks.start(Tasks.taskLIDAR_ControlLEDStrip);
+
+			Schedulers.PeriodicTasks.start(Tasks.taskMeasurePulseSensors);
+		}
+	}
 }
