aeb918a9b0d31989bef2cd960a6610fb9c642c4c
CoryNessCTR <cnessctre@gmail.com>
2018-01-29 15:29:17 -0500

    CANifier Platform.cpp Style Fix
    Fixed style issue where creating a new object was done on a second line.

--- C++/CANifier Demo/src/Platform/Platform.cpp
+++ C++/CANifier Demo/src/Platform/Platform.cpp
@@ -10,14 +10,10 @@ ConcurrentScheduler* Schedulers::PeriodicTasks = new ConcurrentScheduler();
 
 /* Tasks */
 TaskAnimateLEDStrip* Tasks::taskAnimateLEDStrip = new TaskAnimateLEDStrip();
-TaskDirectControlLEDStrip* Tasks::taskDirectControlArm =
-		new TaskDirectControlLEDStrip();
-TaskPWMmotorController* Tasks::taskPWMmotorController =
-		new TaskPWMmotorController();
-TaskMeasurePulseSensors* Tasks::taskMeasurePulseSensors =
-		new TaskMeasurePulseSensors();
-TaskLIDARControlLEDStrip* Tasks::taskLIDARControlLEDStrip =
-		new TaskLIDARControlLEDStrip();
+TaskDirectControlLEDStrip* Tasks::taskDirectControlArm = new TaskDirectControlLEDStrip();
+TaskPWMmotorController* Tasks::taskPWMmotorController =	new TaskPWMmotorController();
+TaskMeasurePulseSensors* Tasks::taskMeasurePulseSensors =	new TaskMeasurePulseSensors();
+TaskLIDARControlLEDStrip* Tasks::taskLIDARControlLEDStrip =	new TaskLIDARControlLEDStrip();
 TaskHSV* Tasks::taskHSVControlLedStrip = new TaskHSV();
 TaskMainLoop* Tasks::taskMainLoop = new TaskMainLoop();
 
