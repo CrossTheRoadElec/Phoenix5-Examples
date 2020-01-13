49c29575bffd8c6112c4b3b6c76a120a2c468d17
CoryNessCTR <cnessctre@gmail.com>
2020-01-13 14:54:40 -0500

    Updated SonarModule to document 7 bit and 8 bit i2c addressing

--- HERO C#/Hero SonarModule Example/Program.cs
+++ HERO C#/Hero SonarModule Example/Program.cs
@@ -11,7 +11,8 @@ namespace Hero_SonarModule
     public class Program
     {
 #if !SETID
-        static SonarModule MySonar = new SonarModule(0x7F, 100);
+		/* Address is bit-shifted to translate from i2c 8 bit address to i2c 7 bit address */
+        static SonarModule MySonar = new SonarModule(0x7F >> 1, 100);
 #endif
 
         public static void Main()
@@ -19,7 +20,11 @@ namespace Hero_SonarModule
 #if SETID
             while (true)
             {
-                SonarModuleAddressChange what = new SonarModuleAddressChange(0x00, 0xFE); //Front is 0xFE, Side is 0xE0 (This is the default for SRF08)
+				/**
+				 * First address gets bit-shifted so that the HERO uses the 7 bit address
+				 * Second address is a parameter that gets passed so it must stay as an 8 bit address
+				 */
+                SonarModuleAddressChange what = new SonarModuleAddressChange(0x00 >> 1, 0xFE /* >> 1 */); //Front is 0xFE, Side is 0xE0 (This is the default for SRF08)
                 Thread.Sleep(10);
             }
 #else
