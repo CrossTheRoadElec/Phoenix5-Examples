a9063fd8ca1a18b3730783ac5548541992b97f39
dadulak <51933047+dadulak@users.noreply.github.com>
2021-04-05 10:40:12 -0400

    EEPROM examples (#36)

---
+++ HERO C#/EEProm Examples/BootCounter Example/Program.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Threading;
+using Microsoft.SPOT;
+using Microsoft.SPOT.Hardware;
+using CTRE.HERO;
+
+namespace Boot_Counter_Example
+{
+	public class Program
+	{
+		static OnboardEEPROM e = OnboardEEPROM.Instance;
+
+
+		public static void Main()
+		{
+
+			while(e.CheckHealth() != CTRE.ErrorCode.OK)//don't continue if CheckHealth is not ok
+			{
+				Debug.Print("CheckHealth() did not return okay");
+			}
+
+			// byte arrays for storing data 
+			byte[] readData = new byte[256];
+			byte[] readData2 = new byte[256];
+			byte[] write = new byte[256];
+			ulong sec1 = 4096;
+			ulong sec2 = 8192;
+			e.Read(sec1, readData);//read sector 1 and store the data 
+			e.Read(sec2, readData2);//read sector 2 and store the data
+			bool chksum = TstChksum(readData);  //test to see if checksum is correct 
+			bool chksum2 = TstChksum(readData2);//test to see if checksum is correct 
+
+
+			if (!chksum&&!chksum2)
+				write[0]= 0;	
+			else if (!chksum&&chksum2)
+				write[0] = readData2[0];
+			else if (!chksum2 && chksum)
+				write[0] = readData[0];
+			else
+			{
+				if (readData[0] < readData2[0])/// check what sector has newer data 
+				{
+					write[0] = readData2[0];
+				}
+				else
+				{
+					write[0] = readData[0];
+				}
+			}
+			
+
+			System.Threading.Thread.Sleep(100);
+
+
+
+			write[0]++;// increment the count 
+			CalcandInsert(write);//insert checksum into bytearray
+			e.Erase4KB(sec1);//erase the first sector
+			e.Write(sec1, write);//write to the first sector
+
+
+			e.Erase4KB(sec2);//erase the second sector
+			e.Write(sec2, write);//write to the second sector
+
+			e.Read(sec1, readData);//read sector 1 
+			e.Read(sec2, readData2);//read sector 2 
+
+			/* loop forever */
+			while (true)
+			{
+				CTRE.Native.Watchdog.Feed(120);// feed so green status light will indicate when everything is complete
+				if (!chksum && !chksum2)
+				{
+					Debug.Print("Both checksums from previous data failed count has been reset");
+				}
+				Debug.Print("boot: " + readData[0]);
+				Debug.Print("boot2 " + readData2[0]);
+
+
+
+				System.Threading.Thread.Sleep(100);
+			}
+		}
+	static 	bool TstChksum( byte[] test)
+		{
+			bool retval = false;
+			int tot = 0;
+			for (int i = 0; i < test.Length; i++)
+			{
+				if (i < test.Length - 1)
+				{
+					tot += test[i];
+
+				}
+				else
+				{
+					if ((byte)tot == test[i])
+					{
+						retval = true;
+					}
+					else
+					{
+						retval = false;
+					}
+				}
+			}
+			return retval;
+		}
+		static void CalcandInsert(byte[] test)
+		{
+			int tot = 0;
+			for (int i = 0; i < test.Length; i++)
+			{
+				if (i < test.Length - 1)
+				{
+					tot += test[i];
+
+				}
+				else
+				{
+					test[i] = (byte)tot;
+				}
+			}
+		}
+	}
+}
---
+++ HERO C#/EEProm Examples/EEPromTest/Program.cs
@@ -0,0 +1,108 @@
+﻿
+//#define ReadOnly
+//Uncomment the line above to only read the data to prove it is persistant across boot cycles 
+using System;
+using System.Threading;
+using Microsoft.SPOT;
+using Microsoft.SPOT.Hardware;
+using CTRE.HERO;
+namespace EEProm
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            OnboardEEPROM e = OnboardEEPROM.Instance;
+            byte[] tofill = new byte[256];
+            byte[] readData = new byte[256];
+
+            for (int i = 0; i < tofill.Length; i++)
+            {
+                tofill[i] = (byte)33;// fill our byte array with values
+            }
+# if(!ReadOnly)
+            for (int sector =0;  sector < 512; sector++)// go through the entire storage  512 * 4kb = the 16mbit storage that the hero has 
+            {
+                int current = sector + 1;
+                Debug.Print("Erasing sector "+ current + "/512");
+
+                e.Erase4KB(GetAddress(sector));//erase 
+                e.Read(GetAddress(sector), readData);//readback erased bytearray to check for erase
+                for(int x = 0; x < readData.Length; x++)
+                {
+                    if (readData[x] != 255)// check that storage has been erased  
+                        Debug.Print("address"+ GetAddress(sector) + " not cleared");// this should never happen
+
+                }
+
+            }
+            // all stored data has now been erased 
+
+            //now lets fill it with new data
+            for (int sector = 0; sector < 512; sector++)// go through the entire storage  512 * 4kb = the 16mbit storage that the hero has 
+            {
+                int current = sector + 1;
+                Debug.Print("filling sector " + current + "/512");
+              
+
+                e.Write(GetAddress(sector),tofill);//fill
+                e.Read(GetAddress(sector), readData);//readback filled bytearray to check for fill
+                for (int x = 0; x < readData.Length; x++)
+                {
+                    if (readData[x] != 33)// check that storage has been filled  
+                        Debug.Print("address" + GetAddress(sector) + "not filled correctly ");// this should never happen
+
+                }
+
+            }
+            //memory has been filled
+#endif
+
+
+            // print all 16mbit of values
+            for (int sector = 0; sector < 512; sector++)// go through the entire storage  512 * 4kb = the 16mbit storage that the hero has 
+            {
+                
+                int current = sector + 1;
+
+                e.Read(GetAddress(sector), readData);//read data 
+                bool allGood = true;
+                for (int x = 0; x < readData.Length; x++)
+                {
+
+                    if (readData[x] != 33)
+                    {
+                        allGood = false;
+                        Debug.Print("printing sector " + current + "/512 " + " address " + GetAddress(sector) + " byte " + x + " value " + readData[x]);
+
+                    }
+
+
+                }
+                if (allGood)
+                {
+                    Debug.Print("all data in sector " + current + "/512 is correct");
+                }
+
+            }// to see that memory is persistant comment out "define ReadOnly at the top and run code"
+            while (true)
+
+            {
+                // do whatever here
+                System.Threading.Thread.Sleep(100);
+            }
+        }
+		static ulong GetAddress(int sector)// returns sector address invalid requests will recive an  address of 0
+		{
+			ulong retval = 0;
+			if (sector == 0)
+				retval = 1;
+			else if (sector > 512)
+				retval = 0;
+			else
+				retval = (ulong)(sector * 4096);
+			return retval;
+		}
+
+	}
+}
