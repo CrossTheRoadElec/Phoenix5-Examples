04d230edb53ac423d69c25181c364e0ce59c7392
LinuxRocks2000 <weird_pusheen@outlook.com>
2022-01-17 18:28:03 -0500

    Fix poor and misleading syntax

--- C++ General/PositionClosedLoop/src/main/cpp/Robot.cpp
+++ C++ General/PositionClosedLoop/src/main/cpp/Robot.cpp
@@ -47,8 +47,8 @@ using namespace frc;
 
 class Robot: public TimedRobot {
 public:
-	TalonSRX * _talon = new WPI_TalonSRX(0);
-	Joystick * _joy = new Joystick(0);
+	WPI_TalonSRX _talon {0};
+	Joystick _joy {0};
 	std::string _sb;
 	int _loops = 0;
 	bool _lastButton1 = false;
@@ -64,31 +64,31 @@ public:
 
 	void RobotInit() {
 		/* Factory Default all hardware to prevent unexpected behaviour */
-		_talon->ConfigFactoryDefault();
+		_talon.ConfigFactoryDefault();
 		
 		/* lets grab the 360 degree position of the MagEncoder's absolute position */
-		int absolutePosition = _talon->GetSensorCollection().GetPulseWidthPosition() & 0xFFF; /* mask out the bottom12 bits, we don't care about the wrap arounds */
+		int absolutePosition = _talon.GetSensorCollection().GetPulseWidthPosition() & 0xFFF; /* mask out the bottom12 bits, we don't care about the wrap arounds */
 		/* use the low level API to set the quad encoder signal */
-		_talon->SetSelectedSensorPosition(absolutePosition, kPIDLoopIdx,
+		_talon.SetSelectedSensorPosition(absolutePosition, kPIDLoopIdx,
 				kTimeoutMs);
 
 		/* choose the sensor and sensor direction */
-		_talon->ConfigSelectedFeedbackSensor(
+		_talon.ConfigSelectedFeedbackSensor(
 				FeedbackDevice::CTRE_MagEncoder_Relative, kPIDLoopIdx,
 				kTimeoutMs);
-		_talon->SetSensorPhase(true);
+		_talon.SetSensorPhase(true);
 
 		/* set the peak and nominal outputs, 12V means full */
-		_talon->ConfigNominalOutputForward(0, kTimeoutMs);
-		_talon->ConfigNominalOutputReverse(0, kTimeoutMs);
-		_talon->ConfigPeakOutputForward(1, kTimeoutMs);
-		_talon->ConfigPeakOutputReverse(-1, kTimeoutMs);
+		_talon.ConfigNominalOutputForward(0, kTimeoutMs);
+		_talon.ConfigNominalOutputReverse(0, kTimeoutMs);
+		_talon.ConfigPeakOutputForward(1, kTimeoutMs);
+		_talon.ConfigPeakOutputReverse(-1, kTimeoutMs);
 
 		/* set closed loop gains in slot0 */
-		_talon->Config_kF(kPIDLoopIdx, 0.0, kTimeoutMs);
-		_talon->Config_kP(kPIDLoopIdx, 0.1, kTimeoutMs);
-		_talon->Config_kI(kPIDLoopIdx, 0.0, kTimeoutMs);
-		_talon->Config_kD(kPIDLoopIdx, 0.0, kTimeoutMs);
+		_talon.Config_kF(kPIDLoopIdx, 0.0, kTimeoutMs);
+		_talon.Config_kP(kPIDLoopIdx, 0.1, kTimeoutMs);
+		_talon.Config_kI(kPIDLoopIdx, 0.0, kTimeoutMs);
+		_talon.Config_kD(kPIDLoopIdx, 0.0, kTimeoutMs);
 	}
 
 	/**
@@ -96,33 +96,33 @@ public:
 	 */
 	void TeleopPeriodic() {
 		/* get gamepad axis */
-		double leftYstick = _joy->GetY();
-		double motorOutput = _talon->GetMotorOutputPercent();
-		bool button1 = _joy->GetRawButton(1);
-		bool button2 = _joy->GetRawButton(2);
+		double leftYstick = _joy.GetY();
+		double motorOutput = _talon.GetMotorOutputPercent();
+		bool button1 = _joy.GetRawButton(1);
+		bool button2 = _joy.GetRawButton(2);
 		/* prepare line to print */
-		_sb.append("\tout:");
-		_sb.append(std::to_string(motorOutput));
-		_sb.append("\tpos:");
-		_sb.append(std::to_string(_talon->GetSelectedSensorPosition(kPIDLoopIdx)));
+		_sb += "\tout:";
+		_sb += std::to_string(motorOutput);
+		_sb += "\tpos:";
+		_sb += std::to_string(_talon.GetSelectedSensorPosition(kPIDLoopIdx));
 		/* on button1 press enter closed-loop mode on target position */
 		if (!_lastButton1 && button1) {
 			/* Position mode - button just pressed */
 			targetPositionRotations = leftYstick * 10.0 * 4096; /* 10 Rotations in either direction */
-			_talon->Set(ControlMode::Position, targetPositionRotations); /* 10 rotations in either direction */
+			_talon.Set(ControlMode::Position, targetPositionRotations); /* 10 rotations in either direction */
 		}
 		/* on button2 just straight drive */
 		if (button2) {
 			/* Percent voltage mode */
-			_talon->Set(ControlMode::PercentOutput, leftYstick);
+			_talon.Set(ControlMode::PercentOutput, leftYstick);
 		}
 		/* if Talon is in position closed-loop, print some more info */
-		if (_talon->GetControlMode() == ControlMode::Position) {
+		if (_talon.GetControlMode() == ControlMode::Position) {
 			/* append more signals to print when in speed mode. */
-			_sb.append("\terrNative:");
-			_sb.append(std::to_string(_talon->GetClosedLoopError(kPIDLoopIdx)));
-			_sb.append("\ttrg:");
-			_sb.append(std::to_string(targetPositionRotations));
+			_sb += "\terrNative:";
+			_sb += std::to_string(_talon.GetClosedLoopError(kPIDLoopIdx)));
+			_sb += "\ttrg:";
+			_sb += std::to_string(targetPositionRotations);
 		}
 		/* print every ten loops, printing too much too fast is generally bad for performance */
 		if (++_loops >= 10) {
